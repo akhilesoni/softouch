74a5dcdaebceada07bbe2e12eee85ab5404bf17f
akhilesoni <akhileshshroff@gmail.com>
2021-04-24 11:19:24 +0530

    first commit

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -12,26 +12,10 @@ var bar3 = document.getElementById('bar3')
 window.addEventListener('scroll',()=>{
     if(window.scrollY>50){
         navbar.style.backgroundColor = 'white'
-        navbar.style.color = 'black'
-        home_option.style.color = 'black'
-        contact_option.style.color = 'black'
-        about_option.style.color = 'black'
-        pricing_option.style.color = 'black'
-
-        bar1.style.backgroundColor = 'black'
-        bar2.style.backgroundColor = 'black'
-        bar3.style.backgroundColor = 'black'
+      
 
     }else{
         navbar.style.backgroundColor = 'transparent'
-        navbar.style.color = 'white'
-        home_option.style.color = 'white'
-        contact_option.style.color = 'white'
-        about_option.style.color = 'white'
-        pricing_option.style.color = 'white'
-
-        bar1.style.backgroundColor = 'white'
-        bar2.style.backgroundColor = 'white'
-        bar3.style.backgroundColor = 'white'
+   
     }
 })
