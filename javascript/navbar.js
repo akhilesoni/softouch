fbdc0f47d056ea744e0e750d0eb5efe39233b785
akhilesoni <akhileshshroff@gmail.com>
2021-04-28 18:35:56 +0530

    new changes

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,20 +1,14 @@
 var navbar = document.getElementById('navbar');
-var home_option = document.getElementById('home_option')
-var pricing_option = document.getElementById('pricing_option')
-var about_option = document.getElementById('about_option')
-var contact_option = document.getElementById('contact_option')
 
-var bar1 = document.getElementById('bar1')
-var bar2 = document.getElementById('bar2')
-var bar3 = document.getElementById('bar3')
 
-var logo = document.getElementById('logo')
 
-var menu_button = document.getElementById('menu_button');
+
+
+var float_menu_button = document.getElementById('float_menu_button');
 var float_menu = document.getElementById('float_menu');
 var cover = document.getElementById('cover')
 
-menu_button.addEventListener('click',()=>{
+float_menu_button.addEventListener('click',()=>{
     float_menu.style.display = 'block';
     cover.style.display = 'block';
     float_menu.style.opacity = 1;
